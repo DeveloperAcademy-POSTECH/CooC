821766e3788dd743b1acd67af585909328b9ab1c
dkdo1406 <54826394+dkdo1406@users.noreply.github.com>
2022-04-12 14:58:13 +0900

    layout,scroll,progress bar완료
    vote ForEach 추가 완료

--- CooC-App/CooC_AppApp.swift
+++ CooC-App/CooC_AppApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct CooC_AppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TopicResultDetailPage()
+//            ContentView()
         }
     }
 }
