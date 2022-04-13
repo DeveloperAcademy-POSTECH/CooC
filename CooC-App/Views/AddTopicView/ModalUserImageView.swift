cd74c5d0a88af0a7be09ccb2ccbdcb82329865cf
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2022-04-13 17:37:25 +0900

    wip

---
+++ CooC-App/Views/AddTopicView/ModalUserImageView.swift
@@ -0,0 +1,43 @@
+//
+//  ModalUserImageView.swift
+//  CooC-App
+//
+//  Created by JungHoonPark on 2022/04/12.
+//
+
+import SwiftUI
+
+struct ModalUserImageView: View {
+    
+    @State private var isShowingPhotoPicker = false
+    // @State private var avatarImage = UIImage(systemName: "camera.fill")!
+    
+    @EnvironmentObject var imagePicker : ImagePicker
+    
+    var body: some View {
+        VStack(){
+            Image(uiImage: imagePicker.avatarImage)
+//                .imageScale(.large)
+                .resizable()
+                .frame(width:128, height: 128)
+                .foregroundColor(Color.orange )
+                .background(Color.gray)
+            //                    .overlay(
+            //                        RoundedRectangle(cornerRadius: 15)
+            //                    )
+                .cornerRadius(10)
+                .padding()
+                .onTapGesture {
+                    print("tapped")
+                   isShowingPhotoPicker = true
+                }
+        }
+        .sheet(isPresented: $isShowingPhotoPicker, content: {PhotoPicker()})
+    }
+}
+
+struct ModalUserImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalUserImageView()
+    }
+}
