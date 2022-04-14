bb52faa69531b6599ba0f9c34ef5ee2252c72b3d
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2022-04-14 14:41:46 +0900

    wip

--- CooC-App/Views/AddTopicView/ModalContents.swift
+++ CooC-App/Views/AddTopicView/ModalContents.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ModalContents: View {
     
-    var categories = ["Fashions", "Tools", "Electronics", "Relationship"]
+    var categories = ["Fashion", "Food", "Game", "Movie","Tools","Life"]
     @State var selectedCategory = 0
     @State var title: String = ""
     @State var Contents: String = ""
@@ -21,51 +21,72 @@ struct ModalContents: View {
             VStack {
                 ModalUserImageView()
                 VStack(alignment: .leading){
-                    Text(/*@START_MENU_TOKEN@*/"제목"/*@END_MENU_TOKEN@*/)
-                        .font(.title2)
+                    Text("Title")
+                        .font(.system(size: 17, weight: .regular))
                         .padding(EdgeInsets(top: 8, leading: 20, bottom: 5, trailing: 0))
-                    TextField("당신의 고민 제목을 적어주세요.", text: $title)
+                    TextField("Please enter the title.", text: $title)
                         .textFieldStyle(.roundedBorder)
                         .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
                     
                     //                            Text("You entered: \(title)")
-                    Text("상세내용")
-                        .font(.title2)
+                    Text("Contents")
+                        .font(.system(size: 17, weight: .regular))
                         .padding(EdgeInsets(top: 0, leading: 20, bottom: 5, trailing: 0))
-                    TextField("당신의 고민 상세 내용을 적어주세요.", text: $Contents)
+                    TextField("Please write somethig here about yourself.", text: $Contents)
                         .textFieldStyle(.roundedBorder)
                         .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
                 }
                 //                VStack(){
                 HStack(){
-                    Text("카테고리")
-                        .font(.title2)
+                    Text("Category")
+                        .font(.system(size: 17, weight: .regular))
                         .padding(EdgeInsets(top: 0, leading: 20, bottom: 5, trailing: 0))
                     Spacer()
                 }
                 VStack(){
-                    Picker("카테고리를 선택해주세요.", selection: $selectedCategory) {
+                    Picker("Please add a category.", selection: $selectedCategory) {
                         ForEach(categories, id:\.self) {
                             Text($0)
                         }
-                    }      .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
-                        .pickerStyle(.inline)
+                    }   .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
+                        .pickerStyle(.menu)    .foregroundColor(ColorManager.mainOrange)
                 }
                 VStack(alignment:.leading){
-                    Text("고민 선택지")
-                        .font(.title2)
-                        .padding(EdgeInsets(top: 0, leading: 20, bottom: 5, trailing: 0))
                     HStack(){
-                        TextField("선택지 1", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                        Text("Choice Options")
+                            .font(.system(size: 17, weight: .regular))
+                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 5, trailing: 0))
+                        Text("Please enter at least two.")
+                            .font(.system(size: 12, weight: .regular))
+                            .foregroundColor(ColorManager.subCobaltBlue)
+                    }
+                    HStack(){
+                        TextField("Edit Option 1", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
                             .multilineTextAlignment(.center)
-                        //                        Color(red: 242/255, green : 157/255, blue: 53/255)
-                        TextField("선택지 2.", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                            .padding(EdgeInsets(top: 0, leading: 25, bottom: 5, trailing: 2))
+                            .textFieldStyle(.roundedBorder)
+                            .font(Font.system(size: 13, design: .default))
+                            .multilineTextAlignment(.center)
+                        //Color(red: 242/255, green : 157/255, blue: 53/255)
+                        TextField("Edit Option 2", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                            .multilineTextAlignment(.center)
+                            .padding(EdgeInsets(top: 0, leading: 2, bottom: 5, trailing: 25))
+                            .textFieldStyle(.roundedBorder)
+                            .font(Font.system(size: 13, design: .default))
                             .multilineTextAlignment(.center)
                     }
                     HStack(){
-                        TextField("선택지 3", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                        TextField("Edit Option 3", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
                             .multilineTextAlignment(.center)
-                        TextField("선택지 4.", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                            .padding(EdgeInsets(top: 0, leading: 25, bottom: 5, trailing: 2))
+                            .textFieldStyle(.roundedBorder)
+                            .font(Font.system(size: 13, design: .default))
+                            .multilineTextAlignment(.center)
+                        TextField("Edit Option 4", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                            .multilineTextAlignment(.center)
+                            .padding(EdgeInsets(top: 0, leading: 2, bottom: 5, trailing: 25))
+                            .textFieldStyle(.roundedBorder)
+                            .font(Font.system(size: 13, design: .default))
                             .multilineTextAlignment(.center)
                         
                     }
@@ -74,16 +95,16 @@ struct ModalContents: View {
                 .frame(maxWidth:.infinity)
             }
             VStack(){
-                DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/, label: { Text("마감기한")
-                    .font(.title2) })
+                DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/, label: { Text("DeadLine")
+                    .font(.system(size: 17, weight: .regular)) })
                 .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
                 Toggle(isOn: $longAnswerToggle) {
-                    Text("주관식 의견 받기")
-                        .font(.title2)
+                    Text("Acceptable in long sentences")
+                        .font(.system(size: 17, weight: .regular))
                 }    .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
                 Toggle(isOn: $personalInfoToggle) {
-                    Text("개인 정보공개 여부")
-                        .font(.title2)
+                    Text("Profile inquiry available")
+                        .font(.system(size: 17, weight: .regular))
                 }    .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
             }
             
